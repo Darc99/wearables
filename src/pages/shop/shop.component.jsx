a129188e95840d4a890fc425563eca1290f9ab45
Darc99 <lennyyhugoh@gmail.com>
2021-01-08 17:10:07 +0100

    deleted some imports in the shop file

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { createStructuredSelector } from 'reselect'
 import { connect } from 'react-redux';
 
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
-import { selectIsCollectionFetching, selectorIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
 
 import CollectionOverviewCotainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container'
-
-import withSpinner from '../../components/with-spinner/with-spinner.component'
-
-import { updateCollections } from '../../redux/shop/shop.actions';
-
-const CollectionPageWithSpinner = withSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
     
@@ -45,4 +37,3 @@ const mapDispatchToProps = dispatch => ({
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
-
