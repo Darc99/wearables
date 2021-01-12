80fd2e93b80f8b83b9a48ff487fe0c9960f0cfb2
Darc99 <lennyyhugoh@gmail.com>
2021-01-12 18:52:58 +0100

    added saga to the shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
 import CollectionOverviewCotainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container'
@@ -10,14 +10,14 @@ import CollectionPageContainer from '../collection/collection.container'
 class ShopPage extends React.Component {
     
     componentDidMount() {
-        const { fetchCollectionsStartAsync } = this.props;
-        fetchCollectionsStartAsync();
+        const { fetchCollectionsStart } = this.props;
+        fetchCollectionsStart();
     }
 
     render() {
         const { match } = this.props;
-        return (
-            <div className='shop-page'>
+        return ( 
+            <div className='shop-page'> 
                 <Route 
                     exact path={`${match.path}`} 
                     component={CollectionOverviewCotainer}
@@ -27,13 +27,13 @@ class ShopPage extends React.Component {
                     component= {CollectionPageContainer}
                 />
             </div>
-        );
+        ); 
     }
 } 
 
 
 const mapDispatchToProps = dispatch => ({
-    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
